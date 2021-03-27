460551ec1750e7e99b4d824652e0e38ee9eee27b
xsh <xsh2668028614@qq.com>
2021-03-28 03:07:25 +0800

    跨域配置

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/config/HospConfig.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/config/HospConfig.java
@@ -3,6 +3,8 @@ package com.atguigu.yygh.hosp.config;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author : xsh
@@ -10,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * @describe:
  */
 @Configuration
-public class HospConfig {
+public class HospConfig extends WebMvcConfigurerAdapter{
 
     /**
      * mybatis-plus 分页插件
@@ -19,5 +21,14 @@ public class HospConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor(){
         return new PaginationInterceptor();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
 }
--- vue-admin-template-master/src/api/hospset.js
+++ vue-admin-template-master/src/api/hospset.js
@@ -6,7 +6,7 @@ export default {
   getHospSetList(current,limit,searchObj){
     return request({
       url:`admin/hosp/hospitalSet/findPageHospSet/${current}/${limit}`,
-      method:'get',
+      method:'post',
       //param:  //param  不使用json传输
       data: searchObj  //data 使用json传输
     })
