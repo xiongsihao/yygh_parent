cefc5258aede8f507bd40913ac8b5f82cb46ad08
xsh <xsh2668028614@qq.com>
2021-03-27 19:08:58 +0800

    增加统一异常处理

---
+++ common/common_util/src/main/java/com/atguigu/yygh/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.atguigu.yygh.common.exception;
+
+import com.atguigu.yygh.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author : xsh
+ * @create : 2021-03-27 - 19:04
+ * @describe:
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+    /**
+     * 自定义异常处理方法
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(YyghException.class)
+    @ResponseBody
+    public Result error(YyghException e){
+        return Result.build(e.getCode(), e.getMessage());
+    }
+
+}
