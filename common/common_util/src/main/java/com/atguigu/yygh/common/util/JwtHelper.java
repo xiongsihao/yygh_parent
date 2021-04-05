86a1f4587412506df6237c692992f27a6c92813c
xsh <xsh2668028614@qq.com>
2021-04-05 22:39:50 +0800

    引入JWT工具类

---
+++ common/common_util/src/main/java/com/atguigu/yygh/common/util/JwtHelper.java
@@ -0,0 +1,48 @@
+package com.atguigu.yygh.common.util;
+
+import io.jsonwebtoken.*;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+/**
+ * @author : xsh
+ * @create : 2021-04-05 - 22:38
+ * @describe:
+ */
+public class JwtHelper {
+    private static long tokenExpiration = 24*60*60*1000;
+    private static String tokenSignKey = "123456";
+
+    public static String createToken(Long userId, String userName) {
+        String token = Jwts.builder()
+                .setSubject("YYGH-USER")
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
+                .claim("userId", userId)
+                .claim("userName", userName)
+                .signWith(SignatureAlgorithm.HS512, tokenSignKey)
+                .compressWith(CompressionCodecs.GZIP)
+                .compact();
+        return token;
+    }
+    public static Long getUserId(String token) {
+        if(StringUtils.isEmpty(token)) return null;
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+        Claims claims = claimsJws.getBody();
+        Integer userId = (Integer)claims.get("userId");
+        return userId.longValue();
+    }
+    public static String getUserName(String token) {
+        if(StringUtils.isEmpty(token)) return "";
+        Jws<Claims> claimsJws
+                = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+        Claims claims = claimsJws.getBody();
+        return (String)claims.get("userName");
+    }
+    public static void main(String[] args) {
+        String token = JwtHelper.createToken(1L, "55");
+        System.out.println(token);
+        System.out.println(JwtHelper.getUserId(token));
+        System.out.println(JwtHelper.getUserName(token));
+    }
+}
