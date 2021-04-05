ca80289e67d0910f25a9d9a46b27b0052002708e
xsh <xsh2668028614@qq.com>
2021-04-05 18:49:13 +0800

    前端预约挂号服务样式文件与布局文件（头部与底部）

--- yygh-site/layouts/default.vue
+++ yygh-site/layouts/default.vue
@@ -1,53 +1,31 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="app-container">
+    <div id="main">
+      <!-- 公共头 -->
+      <myheader/>
+      <div class="main-container">
+        <el-scrollbar class='page-component__scroll'>
+          <!-- 内容区域 -->
+          <nuxt/>
+        </el-scrollbar>
+      </div>
+      <!-- 公共底 -->
+      <myfooter/>
+    </div>
   </div>
 </template>
+<script>
+  import '~/assets/css/app.css'
+  import '~/assets/css/chunk.css'
+  import '~/assets/css/iconfont.css'
+  import '~/assets/css/main.css'
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+  import myheader from './myheader'
+  import myfooter from './myfooter'
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
-
+  export default {
+    components: {
+      myheader, myfooter
+    }
+  }
+</script>
---
+++ yygh-site/layouts/myheader.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header-container">
+    <div class="wrapper">
+      <!-- logo -->
+      <div class="left-wrapper v-link selected">
+        <img style="width: 50px" width="50" height="50" src="~assets/images/logo.png">
+        <span class="text">尚医通 预约挂号统一平台</span>
+      </div>
+      <!-- 搜索框 -->
+      <div class="search-wrapper">
+        <div class="hospital-search animation-show">
+          <el-autocomplete
+            class="search-input small"
+            prefix-icon="el-icon-search"
+            v-model="state"
+            :fetch-suggestions="querySearchAsync"
+            placeholder="点击输入医院名称"
+            @select="handleSelect"
+          >
+            <span slot="suffix" class="search-btn v-link highlight clickable selected">搜索 </span>
+          </el-autocomplete>
+        </div>
+      </div>
+      <!-- 右侧 -->
+      <div class="right-wrapper">
+        <span class="v-link clickable">帮助中心</span>
+        <!--        <el-dropdown >-->
+        <!--              <span class="el-dropdown-link">-->
+        <!--                晴天<i class="el-icon-arrow-down el-icon&#45;&#45;right"></i>-->
+        <!--              </span>-->
+        <!--            <el-dropdown-menu class="user-name-wrapper" slot="dropdown">-->
+        <!--                <el-dropdown-item>挂号订单</el-dropdown-item>-->
+        <!--                <el-dropdown-item>就诊人管理</el-dropdown-item>-->
+        <!--                <el-dropdown-item divided>退出登录</el-dropdown-item>-->
+        <!--            </el-dropdown-menu>-->
+        <!--        </el-dropdown>-->
+        <span class="v-link clickable" @click="dialogUserFormVisible = true">登录/注册</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {}
+</script>
