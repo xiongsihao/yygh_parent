3a04dc82a9438a5e37cf68abdf85dee16f4053bd
xsh <xsh2668028614@qq.com>
2021-04-05 17:18:13 +0800

    增加网关服务service_gateway监听80端口，去除nginx。

---
+++ service_gateway/src/main/java/com/atguigu/yygh/ServerGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : xsh
+ * @create : 2021-04-05 - 17:12
+ * @describe:
+ */
+@SpringBootApplication
+public class ServerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayApplication.class, args);
+    }
+}
