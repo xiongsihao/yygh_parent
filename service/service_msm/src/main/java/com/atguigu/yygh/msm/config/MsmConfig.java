1445dfd3fd4988a1d59ab11c8c76716d8586e0df
xsh <xsh2668028614@qq.com>
2021-04-11 21:41:42 +0800

    短信服务跨域配置

---
+++ service/service_msm/src/main/java/com/atguigu/yygh/msm/config/MsmConfig.java
@@ -0,0 +1,22 @@
+package com.atguigu.yygh.msm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author : xsh
+ * @create : 2021-04-11 - 21:39
+ * @describe:
+ */
+@Configuration
+public class MsmConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
