b7304768f4522d160a3e6bee36281d7698c9715e
xsh <xsh2668028614@qq.com>
2021-04-11 23:17:50 +0800

    用户服务增加跨域配置

--- service/service_user/src/main/java/com/atguigu/yygh/user/config/UserConfig.java
+++ service/service_user/src/main/java/com/atguigu/yygh/user/config/UserConfig.java
@@ -2,6 +2,8 @@ package com.atguigu.yygh.user.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author : xsh
@@ -10,5 +12,14 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @MapperScan("com.atguigu.yygh.user.mapper")
-public class UserConfig {
+public class UserConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
 }
