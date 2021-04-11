f2dd1f368ecfd653aaa08849f8cd8bccdbc3030d
xsh <xsh2668028614@qq.com>
2021-04-11 23:11:11 +0800

    网关增加全局登录校验

--- yygh-site/utils/request.js
+++ yygh-site/utils/request.js
@@ -1,34 +1,47 @@
-import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import axios from 'axios'
+import {MessageBox, Message} from 'element-ui'
+import cookie from 'js-cookie'
+
 // 创建axios实例
-const service = axios.create({
-  baseURL: 'http://localhost',
-  timeout: 15000 // 请求超时时间
+const service = axios.create({
+  baseURL: 'http://localhost',
+  timeout: 15000 // 请求超时时间
 })
 // http request 拦截器
 service.interceptors.request.use(
-  config => {
-    // token 先不处理，后续使用时在完善
-    return config
+  config => {
+    //判断cookie是否有token值
+    if (cookie.get('token')) {
+      //token值放到cookie里面
+      config.headers['token'] = cookie.get('token')
+    }
+    return config
   },
-  err => {
-    return Promise.reject(err)
+  err => {
+    return Promise.reject(err)
   })
 // http response 拦截器
 service.interceptors.response.use(
-  response => {
-    if (response.data.code !== 200) {
-      Message({
-        message: response.data.message,
-        type: 'error',
-        duration: 5 * 1000
-      })
-      return Promise.reject(response.data)
-    } else {
-      return response.data
+  response => {
+    //状态码是208
+    if (response.data.code === 208) {
+      //弹出登录输入框
+      loginEvent.$emit('loginDialogEvent')
+      return
+    } else {
+      if (response.data.code !== 200) {
+        Message({
+          message: response.data.message,
+          type: 'error',
+          duration: 5 * 1000
+        })
+        return Promise.reject(response.data)
+      } else {
+        return response.data
+      }
     }
   },
-  error => {
-    return Promise.reject(error.response)
+  error => {
+    return Promise.reject(error.response)
   })
-export default service
+export default service
