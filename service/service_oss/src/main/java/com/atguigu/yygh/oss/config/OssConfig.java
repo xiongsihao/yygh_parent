f9c88a984de4007c95180dab04ab13cf704f5955
xsh <xsh2668028614@qq.com>
2021-04-13 02:41:20 +0800

    OSS服务跨域配置

---
+++ service/service_oss/src/main/java/com/atguigu/yygh/oss/config/OssConfig.java
@@ -0,0 +1,24 @@
+package com.atguigu.yygh.oss.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author : xsh
+ * @create : 2021-04-05 - 22:18
+ * @describe:
+ */
+@Configuration
+public class OssConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
