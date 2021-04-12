e4300528c3b5fe167549a62339425c9b208fb4ad
xsh <xsh2668028614@qq.com>
2021-04-13 02:42:21 +0800

    前端工程增加实名认证接口

--- yygh-site/api/userInfo.js
+++ yygh-site/api/userInfo.js
@@ -9,5 +9,19 @@ export default {
       method: `post`,
       data: userInfo
     })
+  },
+  getUserInfo() {
+    return request({
+      url: `${api_name}/auth/getUserInfo`,
+      method: `get`
+    })
+  },
+
+  saveUserAuah(userAuah) {
+    return request({
+      url: `${api_name}/auth/userAuah`,
+      method: 'post',
+      data: userAuah
+    })
   }
 }
