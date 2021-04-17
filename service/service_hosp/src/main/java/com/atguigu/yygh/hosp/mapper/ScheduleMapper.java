a03f5b3c70284cc310d7de920c66ab86a01cbc49
xsh <xsh2668028614@qq.com>
2021-04-18 02:04:44 +0800

    @Mapper

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/mapper/ScheduleMapper.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/mapper/ScheduleMapper.java
@@ -2,6 +2,8 @@ package com.atguigu.yygh.hosp.mapper;
 
 import com.atguigu.yygh.model.hosp.Schedule;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ScheduleMapper extends BaseMapper<Schedule> {
 }
--- service/service_order/src/main/java/com/atguigu/yygh/order/mapper/OrderInfoMapper.java
+++ service/service_order/src/main/java/com/atguigu/yygh/order/mapper/OrderInfoMapper.java
@@ -2,11 +2,13 @@ package com.atguigu.yygh.order.mapper;
 
 import com.atguigu.yygh.model.order.OrderInfo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author : xsh
  * @create : 2021-04-14 - 1:10
  * @describe:
  */
+@Mapper
 public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
 }
