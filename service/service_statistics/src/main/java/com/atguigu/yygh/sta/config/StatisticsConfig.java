f73f0ccd6da7c153372e6511fb7ff17e921e16bd
xsh <xsh2668028614@qq.com>
2021-04-19 02:09:29 +0800

    数据统计服务网关和跨域配置

---
+++ service/service_statistics/src/main/java/com/atguigu/yygh/sta/config/StatisticsConfig.java
@@ -0,0 +1,23 @@
+package com.atguigu.yygh.sta.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author : xsh
+ * @create : 2021-04-05 - 22:18
+ * @describe:
+ */
+@Configuration
+public class StatisticsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
